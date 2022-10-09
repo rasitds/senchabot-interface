7219d5706e7e8e9d0f82121340c2ec5e0539e0f8
Rasit <9775791-rasitxy@users.noreply.gitlab.com>
2022-10-09 13:36:22 +0300

    Add Command Aliases #3

--- src/commands/CommandRegistry.ts
+++ src/commands/CommandRegistry.ts
@@ -5,45 +5,47 @@ import { ICommand } from "./ICommand";
 import { InitializePrintCommand } from "./PrintCommand";
 import { InitializeThemeCommand } from "./ThemeCommand";
 
-export class CommandRegistry  {
-    private _Commands : ICommand[] = [];
-    public get Commands() : ICommand[] {
-        return this._Commands;
-    }
-        
-    public addCommand(command: ICommand) : void {
-        this._Commands.push(command);
-    }
+export class CommandRegistry {
+  private _Commands: ICommand[] = [];
+  public get Commands(): ICommand[] {
+    return this._Commands;
+  }
 
-    public getCommand(name: string) : ICommand | undefined {
-        return this._Commands.find(c => c.name === name);
-    }
-    
-    private static _instance: CommandRegistry;
-    public static get instance() : CommandRegistry {
-        if (CommandRegistry._instance === undefined)
-            CommandRegistry._instance = new CommandRegistry();
-        return CommandRegistry._instance;
-    }
+  public addCommand(command: ICommand): void {
+    this._Commands.push(command);
+  }
 
-    public static AddCommand(command: ICommand) : void {
-        if(CommandRegistry.instance.getCommand(command.name) === undefined)
-            CommandRegistry.instance.addCommand(command);
-    }
+  public getCommand(name: string): ICommand | undefined {
+    return this._Commands.find(
+      (c) => c.name === name || c.aliases?.includes(name)
+    );
+  }
 
-    public static getCommand(name: string) : ICommand | undefined {
-        return CommandRegistry.instance.getCommand(name);
-    }
+  private static _instance: CommandRegistry;
+  public static get instance(): CommandRegistry {
+    if (CommandRegistry._instance === undefined)
+      CommandRegistry._instance = new CommandRegistry();
+    return CommandRegistry._instance;
+  }
 
-    public static Commands() : ICommand[] {
-        return CommandRegistry.instance.Commands;
-    }
+  public static AddCommand(command: ICommand): void {
+    if (CommandRegistry.instance.getCommand(command.name) === undefined)
+      CommandRegistry.instance.addCommand(command);
+  }
 
-    public static registerAllCommands() : void {
-        CommandRegistry.AddCommand(InitializeColorCommand());
-        CommandRegistry.AddCommand(InitializeThemeCommand());
-        CommandRegistry.AddCommand(InitializePrintCommand());
-        CommandRegistry.AddCommand(InitializeFullscreenCommand());
-        CommandRegistry.AddCommand(InitializeCmdListCommand());
-    }
+  public static getCommand(name: string): ICommand | undefined {
+    return CommandRegistry.instance.getCommand(name);
+  }
+
+  public static Commands(): ICommand[] {
+    return CommandRegistry.instance.Commands;
+  }
+
+  public static registerAllCommands(): void {
+    CommandRegistry.AddCommand(InitializeColorCommand());
+    CommandRegistry.AddCommand(InitializeThemeCommand());
+    CommandRegistry.AddCommand(InitializePrintCommand());
+    CommandRegistry.AddCommand(InitializeFullscreenCommand());
+    CommandRegistry.AddCommand(InitializeCmdListCommand());
+  }
 }
