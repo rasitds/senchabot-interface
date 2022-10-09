2f4207c657445deb3953a939243e5102ff145c9b
Rasit <9775791-rasitxy@users.noreply.gitlab.com>
2022-10-09 13:36:22 +0300

    Add command alias #3

--- src/commands/ICommand.ts
+++ src/commands/ICommand.ts
@@ -1,9 +1,10 @@
 export interface ICommand {
-    name: string,
-    execute(args: string): void,
-    help() : void,
-    description?: string,
-    usage?: string,
-    hidden?: boolean,
-    admin?: boolean
+  name: string;
+  aliases?: string[];
+  execute(args: string): void;
+  help(): void;
+  description?: string;
+  usage?: string;
+  hidden?: boolean;
+  admin?: boolean;
 }
--- src/commands/PrintCommand.ts
+++ src/commands/PrintCommand.ts
@@ -2,44 +2,49 @@ import { useResponseContext } from "../contexts/ResponseContext";
 import { AnyContextType } from "../types";
 import { ICommand } from "./ICommand";
 
-export function InitializePrintCommand() : ICommand {
-    const responseContext: AnyContextType = useResponseContext();
-    return new PrintCommand(responseContext);
+export function InitializePrintCommand(): ICommand {
+  const responseContext: AnyContextType = useResponseContext();
+  return new PrintCommand(responseContext);
 }
 
 export class PrintCommand implements ICommand {
-    public name: string = 'print';
-    public params?: string[] = [ "/", "_" ];
-    private responseContext: AnyContextType;
+  public name: string = "print";
+  public aliases?: string[] | undefined = ["echo"];
+  public params?: string[] = ["/", "_"];
+  private responseContext: AnyContextType;
 
-    private readonly InvalidParameterMessage = "Invalid parameter. Valid parameters are: /, _";
+  private readonly InvalidParameterMessage =
+    "Invalid parameter. Valid parameters are: /, _";
 
-    constructor(responseContext: AnyContextType) {
-        this.responseContext = responseContext;
-    }
+  constructor(responseContext: AnyContextType) {
+    this.responseContext = responseContext;
+  }
 
-    public execute(parameters: string) {
-        let args: string[] = parameters.split(' ');
-        let type: string = args.shift() || "";
+  public execute(parameters: string) {
+    let args: string[] = parameters.split(" ");
+    let type: string = args.shift() || "";
 
-        if (!this.params?.includes(type))
-            return this.setResponseState("PARAMETER ERROR", this.InvalidParameterMessage);
+    if (!this.params?.includes(type))
+      return this.setResponseState(
+        "PARAMETER ERROR",
+        this.InvalidParameterMessage
+      );
 
-        var booleanType: boolean = type === "/" ? false : true;
-        let message: string = args.join(' ');
-        
-        if (booleanType)
-            this.setResponseState(message, undefined);
-        else
-            this.setResponseState("....", message);
-    }
+    var booleanType: boolean = type === "/" ? false : true;
+    let message: string = args.join(" ");
 
-    setResponseState(message?: string, error?: string) {
-        this.responseContext.setResponseState({ lineText: message, outputText: [ error ] });
-    }
+    if (booleanType) this.setResponseState(message, undefined);
+    else this.setResponseState("....", message);
+  }
 
-    public help(): void {
-    }
+  setResponseState(message?: string, error?: string) {
+    this.responseContext.setResponseState({
+      lineText: message,
+      outputText: [error],
+    });
+  }
 
-    public usage?: string = "/print </|_> <text>";
+  public help(): void {}
+
+  public usage?: string = "/print </|_> <text>";
 }
