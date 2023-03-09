782204a8bebaeb908ecef4fff8a15d01982aecc9
emredevsalot <72770244+emredevsalot@users.noreply.github.com>
2023-03-09 22:24:57 +0300

    add command not found (#23)
    * Print "Command not found." message when typed comment is not available.
    
    * Added "Please start command with /" if input doesn't start with "/"
    
    * Fixed "Please start command with /" response

--- src/components/InputManager.tsx
+++ src/components/InputManager.tsx
@@ -64,7 +64,13 @@ export const InputManager = ({ isInputOpen }: { isInputOpen: boolean }) => {
       }
 
       setResponseState({
-        lineText: " " + lineText.toUpperCase(),
+        lineText: "Command not found." + lineText.toUpperCase(),
+        outputText: outputText,
+      });
+    } else {
+      
+      setResponseState({
+        lineText: "Please start command with /" + lineText.toUpperCase(),
         outputText: outputText,
       });
     }
